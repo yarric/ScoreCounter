1bf5aa6f148959a4e5f42d72281876de9b027ea4
Yaric Shkurat <yarrik@yarics-macbook-pro.local>
2018-02-16 20:36:39 +0300

    Updated for xCode

--- ScoreCounter/ViewController.swift
+++ ScoreCounter/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func addUpperTapped(_ sender: Any) {
-        upperHealthCount += 1 // add one
+        upperHealthCount += 1
         updateUI()
     }
     
